d7a4c3cf790349f54ea7c07e4f81240d3c2b78a5
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-08-02 11:03:42 -0400

    Add internal version of pos-js. Add test command

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// Speakeasy
-//
-// A simple natural language processor that tries to be 
-// extremely accurate 80% of the time.
-// 
-// Note: I am not a linguist, this is the result of
-// writing whatever logic it takes to get my tests
-// to work.
-//
-// Please help me make this better:
-// https://github.com/nhunzaker/speakeasy
-// -------------------------------------------------- //
+/**
+ * Speakeasy
+ *
+ * A simple natural language processor that tries to be
+ * extremely accurate 80% of the time.
+ *
+ * Note: I am not a linguist, this is the result of
+ * writing whatever logic it takes to get my tests
+ * to work.
+ *
+ * Please help me make this better:
+ * https://github.com/nhunzaker/speakeasy
+ */
 
 module.exports = {
     sentiment : require("./lib/sentiment"),
